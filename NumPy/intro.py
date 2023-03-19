03e265ab82e8e220c0ec9c5520046194f0595d3e
ma-curle <adi@gmail.com>
2023-03-19 09:38:57 +0545

    learn about npdim and array dimesions

--- NumPy/intro.py
+++ NumPy/intro.py
@@ -8,7 +8,30 @@
 # to install numpy : pip install numpy
 
 import numpy as np
-
-arr= np.array ([1,2,3,4,5,6,7,8,9])
-
+arr= np.array([1,2,3,4,5,6,7,8,9])
 print(type(arr))
+
+
+# 0-D array
+
+a=np.array(24)
+print(a)
+
+# 1-D array
+arr = np.array([1,2,3,4,5])
+print(arr)
+print(arr[1])
+
+# 2-D array
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr)
+print(arr[1,1])  # this will print the value 5 from second bracket
+
+
+# 3-D array
+
+arr=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr)
+print(arr[1,0,1])
+print(a.ndim)  # .ndim is to show the dimesion of arrays
+print(arr.ndim)
