342f79f16fd6cd2784cf3114cf098e07e8f9cce3
ma-curle <adi@gmail.com>
2023-03-19 09:57:39 +0545

    short notes on data types

--- NumPy/datatype.py
+++ NumPy/datatype.py
@@ -1,1 +1,4 @@
 #string , integer, float, bollean, complex etc are the datatypes of numpy
+# i=> integer, b=> boolean, u=>unsigned integer, f=> floate, c =>complex float
+# m = timedelta, M=>datetime, O =<Object, s=>string, u=>unicode string
+#fixed chunk of memory for others data type (void)
